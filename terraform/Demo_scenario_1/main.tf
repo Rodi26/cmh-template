9e04a0ca17f04e735e8c5b4669c4c6fcf2909642
rodololphe.fontaine@fr.ibm.com <rodololphe.fontaine@fr.ibm.com>
2018-05-25 14:38:23 +0000

    maj des valeurs par défault

--- terraform/Demo_scenario_1/main.tf
+++ terraform/Demo_scenario_1/main.tf
@@ -41,6 +41,7 @@ resource "ibm_compute_vm_instance" "server-db" {
   os_reference_code = "${var.server-db_os_reference_code}"
   public_vlan_id       = "${var._public_vlan_id}"
   private_vlan_id       = "${var._private_vlan_id}"
+  private_security_group_ids = ["${var.group_name_private_id}"]
 }
 
 resource "tls_private_key" "ssh" {
